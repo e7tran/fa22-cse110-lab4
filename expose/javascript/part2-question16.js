22835e7e95633efef8e57ad71ccf654667223695
Eric Tran <93348111+e7tran@users.noreply.github.com>
2022-10-20 14:14:59 -0700

    Update part2-question16.js

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,1 +1,18 @@
+let statistics = {
+  redCars: 21,
+  blueCars: 45,
+  greenCars: 12,
+  raceCars: 5,
+  blackCars: 40,
+  rareCars: 2
+}; // Object to iterate over
 
+
+for(const car in statistics) {
+  if(car[0] == 'r') {
+    console.log(statistics[car]); 
+  } //Print value of property if starts with r
+  else if(statistics[car] % 2 == 1) {
+    console.log(statistics[car]);
+  } //Print value of property if value of property odd
+}
