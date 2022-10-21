95cc7018cdefa4ec02c7b20bdee0d54135db3cdf
Eric Tran <93348111+e7tran@users.noreply.github.com>
2022-10-20 21:13:05 -0700

    Update part2-question18.js

--- expose/javascript/part2-question18.js
+++ expose/javascript/part2-question18.js
@@ -1,1 +1,7 @@
+function printNum() {
+  let d = new Date();
+  let time = d.toLocaleTimeString();
+  console.log(time);
+} //Function to print time
 
+setInterval(printNum, 1000); //printNum every second
